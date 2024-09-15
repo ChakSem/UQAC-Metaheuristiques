149bad4343b2e899ae9cb35fac0af49c273fdc27
ChakSemm <alaa.chakori.semmane@gmail.com>
2024-09-15 11:30:28 -0400

    Lab1 - Push du projet sur Moodle

---
+++ LaboDescente etudiants/execution/execution.bat
@@ -0,0 +1,48 @@
+REM L'executable (LaboDescente.exe) ainsi que les fichiers de la DLL (DescenteDLL.dll et DescenteDLL.lib)
+REM  doivent etre dans le meme repertoire que le fichier .txt
+
+REM Executables et DLL actuels sont en RELEASE 
+
+REM Liste des paramètres
+REM 0)EXECUTABLE 1)NOM_FICHIER 2)TAILLE_VOISINAGE 3)NB_EVAL_MAX
+LaboDescente.exe	fic12.txt	1 	100000
+LaboDescente.exe	fic12.txt	1 	100000
+LaboDescente.exe	fic12.txt	1 	100000
+LaboDescente.exe	fic12.txt	1 	100000
+LaboDescente.exe	fic12.txt	1 	100000
+
+LaboDescente.exe	fic15a.txt	1 	100000
+LaboDescente.exe	fic15a.txt	1 	100000
+LaboDescente.exe	fic15a.txt	1 	100000
+LaboDescente.exe	fic15a.txt	1 	100000
+LaboDescente.exe	fic15a.txt	1 	100000
+
+LaboDescente.exe	fic15b.txt	1 	100000
+LaboDescente.exe	fic15b.txt	1 	100000
+LaboDescente.exe	fic15b.txt	1 	100000
+LaboDescente.exe	fic15b.txt	1 	100000
+LaboDescente.exe	fic15b.txt	1 	100000
+
+LaboDescente.exe	fic25a.txt	1 	100000
+LaboDescente.exe	fic25a.txt	1 	100000
+LaboDescente.exe	fic25a.txt	1 	100000
+LaboDescente.exe	fic25a.txt	1 	100000
+LaboDescente.exe	fic25a.txt	1 	100000
+
+LaboDescente.exe	fic25b.txt	1 	100000
+LaboDescente.exe	fic25b.txt	1 	100000
+LaboDescente.exe	fic25b.txt	1 	100000
+LaboDescente.exe	fic25b.txt	1 	100000
+LaboDescente.exe	fic25b.txt	1 	100000
+
+LaboDescente.exe	fic35a.txt	1 	100000
+LaboDescente.exe	fic35a.txt	1 	100000
+LaboDescente.exe	fic35a.txt	1 	100000
+LaboDescente.exe	fic35a.txt	1 	100000
+LaboDescente.exe	fic35a.txt	1 	100000
+
+LaboDescente.exe	fic35b.txt	1 	100000
+LaboDescente.exe	fic35b.txt	1 	100000
+LaboDescente.exe	fic35b.txt	1 	100000
+LaboDescente.exe	fic35b.txt	1 	100000
+LaboDescente.exe	fic35b.txt	1 	100000
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # UQAC-Metaheuristiques
+
